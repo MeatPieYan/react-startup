038054d2502b60373982fed6a5fc9db822dc8b3c
YanZhenggang <yanzhenggang@zhongan.com>
2017-12-25 15:52:49 +0800

    add gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 const gulp = require('gulp');
+const babel = require('gulp-babel');
+const gulpif = require('gulp-if');
 
 gulp.task('default', () => {
+  gulp.src('./shared/**/*')
+    .pipe(gulpif((file) => (file.history[0].endsWith('.js')), babel()))
+    .pipe(gulp.dest('./dist/shared'));
 
+  gulp.src('./server/**/*')
+    .pipe(gulpif((file) => (file.history[0].endsWith('.js')), babel()))
+    .pipe(gulp.dest('./dist/server'));
 });
